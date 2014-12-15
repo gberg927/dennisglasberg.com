d8d579969ff461d1e340f1252251f8c6f8cd7294
Dennis Glasberg <dglasberg@asascience.com>
2014-12-15 11:06:12 -0500

    added theme to CI

---
+++ public/application/views/home.php
@@ -0,0 +1,72 @@
+
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Dennis Glasberg</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Dennis Glasberg Landing Page">
+		<meta name="author" content="Dennis Glasberg">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
+		<link rel="icon" href="../../favicon.ico">
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css" rel="stylesheet">
+		<link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+		<link href="css/style.css" rel="stylesheet">
+	</head>
+
+	<body>
+		<div class="container">
+			<div class="header">
+				<h3 class="masthead-brand">Dennis Glasberg</h3>
+				<nav>
+					<ul class="nav masthead-nav">
+						<li class="active"><a href="index.html">Home</a></li>
+						<li><a href="about.html">About</a></li>
+						<li><a href="contact.html">Contact</a></li>
+					</ul>
+				</nav>
+			</div>
+		</div>
+		<div class="container inner">
+			<h1>dennis glasberg</h1>
+			<p>programmer, hip-hop producer</p>
+            <div class="row">
+                <div class="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <ul class="list-inline text-center">
+                        <li>
+                            <a href="http://github.com/gberg927">
+                                <span class="fa-stack fa-lg social">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-github fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="http://www.linkedin.com/in/dennisglasberg">
+                                <span class="fa-stack fa-lg social">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-linkedin fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="https://twitter.com/nicemancometh">
+                                <span class="fa-stack fa-lg social">
+                                    <i class="fa fa-circle fa-stack-2x"></i>
+                                    <i class="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+		</div>
+		<div class="container">
+			<div class="mastfoot">
+          		<p>&copy; Dennis Glasberg 2014</p>
+          </div>
+		</div>
+	</body>
+</html>
