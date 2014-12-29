9b424b96ec4816aa509f1ca6c8fecaf4633dc523
Dennis Glasberg <dglasberg@asascience.com>
2014-12-29 16:18:28 -0500

    fixed countdown, added map

--- public/application/views/test.php
+++ public/application/views/test.php
@@ -6,23 +6,13 @@
 <script src="http://cdn.leafletjs.com/leaflet-0.7/leaflet.js"></script>
 <script src='https://api.tiles.mapbox.com/mapbox.js/v1.6.4/mapbox.js'></script>
 
+<style>
+    
+</style>
 <div class="container inner">
     <div class="container">
-		<h1>Cruise!!!</h1>
+        <div id="map" class="container"></div>
     </div>
-    <div id="map" class="container"></div>
-	<div class="cruise-clock container" style="padding-top: 15px"></div>
 </div>
-
-<script>
-	var date  = new Date(2015, 0, 17, 21, 0, 0);
-	var now   = new Date();
-	var diff  = date.getTime() / 1000 - now.getTime() / 1000;
-
-	var clock = $('.cruise-clock').FlipClock(diff, {
-		clockFace: 'DailyCounter',
-		countdown: true
-	});
-</script>
 
 <script src="<?php echo base_url(); ?>js/map.js"></script>
